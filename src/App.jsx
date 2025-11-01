ff196e78cd272cadec953234a83a4f1c7ae0374a
MichelleMOU86 <Michelle.L.Munoz@gmail.com>
2025-11-01 17:23:07 -0400

    Make corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Weather app</h1>
