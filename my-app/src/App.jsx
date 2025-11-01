e78f0ae056829375db5bc6d95cbb5f052f3c30fa
MichelleMOU86 <Michelle.L.Munoz@gmail.com>
2025-11-01 16:16:24 -0400

    Added GitHub repo URL

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,5 +4,19 @@ import "./App.css";
 export default function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by Michelle Wilson and is open-sourced on{" "}
+        <a
+          href="https://github.com/MichelleMOU86/react-weather-app-reset"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
